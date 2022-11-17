c24cb7828923ae68cba6ac1637c02cd4c233e10a
anna <annaqiu527@gmail.com>
2022-11-18 10:28:27 +1300

    changes on components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,8 +3,10 @@ import { Route, Routes } from 'react-router-dom'
 
 import Nav from './Nav'
 import Home from './Home'
-import Month from './Month'
-import Country from './Country'
+import Months from './Months'
+import Days from './Days'
+// import continents from '../../data/continents'
+// import countries from '../../data/countries'
 
 function App() {
   return (
@@ -14,15 +16,15 @@ function App() {
           src="/images/color_earth.gif"
           alt="A coloured globe of the earth spinning around on its axis"
         />
-        <h1>Navigating the worldwide routes</h1>
+        <h1>Welcome to the cool days worldwide</h1>
       </div>
       {/* This 'main' div is only for styling (so we can use flexbox) */}
       <div className="main">
         <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/months/:name" element={<Month />} />
-          <Route path="/month/:name/:code" element={<Country />} />
+          <Route path="/continents/:name" element={<Months />} />
+          <Route path="/continent/:name/:code" element={<Days />} />
         </Routes>
       </div>
     </>
